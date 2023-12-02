56a4dec4aefc5696209f4b6933dab4e77df60b04
Sean Jensen <itskolesean@gmail.com>
2023-12-02 14:39:54 +0100

    Initial setup

--- mcu/src/main.cpp
+++ mcu/src/main.cpp
@@ -11,4 +11,10 @@ void setup() {
 
 void loop() {
   // put your main code here, to run repeatedly:
+  static int count = 0;
+  Logger.Info("Loop Iteration");
+  Logger.Info("Loop [%d]", count);
+  Logger.Error("Loop [%d]", count);
+
+  count++;
 }
