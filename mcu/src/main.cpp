f52897645625a034c346eb2d89e623a8fbe4b7a5
Sean Jensen <itskolesean@gmail.com>
2023-12-07 21:11:30 +0100

    some clean up

--- mcu/src/main.cpp
+++ mcu/src/main.cpp
@@ -8,6 +8,8 @@
 #include "secrets.h"
 #include <cstring>
 
+#include <MQTT.hpp>
+
 #define SERIAL_LOGGER_BAUD_RATE 115200
 
 #include <ArduinoJson.h>
@@ -51,69 +53,12 @@ bool TryGetValue(JsonObject obj, const char* key, T* value)
   return false;
 }
 
-StaticJsonDocument<8192> doc; // Create a StaticJsonDocument with a capacity of 512 bytes. Adjust if necessary.
-
-void callback(char* topic, byte* payload, unsigned int length) {
-  Logger.Info("Message arrived [%s]", topic);
-  Logger.Info("Payload: %s", payload);
-
-  // TWO IMAGES
-  DeserializationError error = deserializeJson(doc, payload, length); // Parse the payload
-
-  if (error)
-  { // Test if parsing succeeds.
-    Logger.Info("deserializeJson() failed with code %s", error.c_str());
-    return;
-  }
-
-  JsonObject obj = doc.as<JsonObject>(); // Get the JsonObject from the JsonDocument.
-
-  // Now you can use the JsonObject.
-  // For example, let's print it to the serial:
-  serializeJson(obj, Serial);
-  if (strcmp(topic, "matrix/text") == 0)
-  {
-      Logger.Info("Text");
-      char text[512];
-      if (TryGetString(obj, "payload", text))
-      {
-        Logger.Info("Text: %s", text);
-      }
-
-      int color;
-      if (TryGetValue(obj, "color", &color))
-      {
-        Logger.Info("Color: %d", color);
-      }
-
-      Matrix::SetText(static_cast<uint16_t>(color), text);
-  }
-  else if (strcmp(topic, "matrix/image") == 0)
-  {
-    Logger.Info("Image");
-    // Need to set image
-    uint16_t* image = new uint16_t[16 * 16];
-    if (TryGetUInt16Array(obj, "payload", image, 16 * 16))
-    {
-      Logger.Info("Image: %d", image);
-      Matrix::SetImage(image);
-    }
-  }
-  else
-  {
-    Logger.Info("Unknown message");
-  }
-}
-
-
 WiFiClient espClient;
-PubSubClient client(espClient);
 
 void setup() {
   SerialLogger::Begin();
 
   delay(2000); // Wait for serial to work, so the initial message is displayed.
-  client.setBufferSize(4096);
 
   Logger.Info("Serial started at BAUD[%d]", SERIAL_LOGGER_BAUD_RATE);
 
@@ -127,25 +72,50 @@ void setup() {
     Logger.Info("Connecting to WiFi..");
   }
 
-  // TODO: Setup MQTT
-  client.setServer(MQTT_BROKER, MQTT_PORT);
-  client.setCallback(callback);
-  while (!client.connected()) {
-    Logger.Info("Connecting to MQTT...");
-    String client_id = "esp32-client-";
-    client_id += String(WiFi.macAddress());
-    if (client.connect(client_id.c_str())) {
-      Logger.Info("connected");
-    } else {
-      Logger.Info("failed with state %d", client.state());
-      delay(2000);
+  MQTT::AddTopicHandler("matrix/scale", [](const JsonObject& obj) {
+    Logger.Info("Scale");
+    uint16_t width, height;
+    if (TryGetValue(obj, "width", &width) && TryGetValue(obj, "height", &height))
+    {
+      Matrix::Scale(width, height);
     }
-  }
-  client.subscribe("matrix/+");
+  });
+
+  MQTT::AddTopicHandler("matrix/text", [](const JsonObject& obj) {
+    Logger.Info("Text");
+    char text[512];
+    if (TryGetString(obj, "payload", text))
+    {
+      Logger.Info("Text: %s", text);
+    }
+
+    int color;
+    if (TryGetValue(obj, "color", &color))
+    {
+      Logger.Info("Color: %d", color);
+    }
+
+    Matrix::SetText(static_cast<uint16_t>(color), text);
+  });
+
+  MQTT::AddTopicHandler("matrix/image", [](const JsonObject& obj) {
+    Logger.Info("Image");
+    // Need to set image
+    // TODO: This needs update -> we can scale..
+    uint16_t* image = new uint16_t[16 * 16];
+    if (TryGetUInt16Array(obj, "payload", image, 16 * 16))
+    {
+      Logger.Info("Image: %d", image);
+      Matrix::SetImage(image);
+    }
+  });
+
+  // TODO: Setup MQTT
+  MQTT::Connect(espClient, "matrix/+");
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  client.loop();
+  MQTT::Loop();
   Matrix::Loop();
 }
