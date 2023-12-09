cea7ff3b88e82424eade3b05d09faab56e842fcb
Sean Jensen <itskolesean@gmail.com>
2023-12-09 15:28:49 +0100

    sick

--- matrix-panel-app/app/(tabs)/register.tsx
+++ matrix-panel-app/app/(tabs)/register.tsx
@@ -6,99 +6,125 @@ import { useEffect, useState } from 'react';
 
 import { Text, View } from '../../components/Themed';
 import Colors from '../../constants/Colors';
-
-interface MatrixConfig {
-  name: string;
-  height: number;
-  width: number;
-  brokerUrl: string;
-  brokerPort: number;
-}
+import { VStack } from '../../components/VStack';
+import { SignboardConfig } from '../../types/MatrixConfig';
+import { HStack } from '../../components/HStack';
 
 export default function RegisterScreen() {
   const colorScheme = useColorScheme();
 
-  const [matrixConfig, setMatrixConfig] = useState<MatrixConfig>({} as MatrixConfig)
+  const [matrixConfig, setMatrixConfig] = useState<SignboardConfig>({
+    width: 16,
+    height: 16,
+    brokerPort: 8883,
+    brokerScheme: 'https://'
+  } as SignboardConfig)
 
-  function SetConfig<T extends keyof MatrixConfig>(key: T, value: MatrixConfig[T]) {
-      setMatrixConfig(prev => ({...prev, [key]: value}));
-      console.log("MatrixConfig: ", matrixConfig);
-      console.log(matrixConfig);
+  function SetConfig<T extends keyof SignboardConfig>(key: T, value: SignboardConfig[T]) {
+    setMatrixConfig(prev => ({...prev, [key]: value}));
+    console.log("MatrixConfig: ", matrixConfig);
+    console.log(matrixConfig);
   }
-
-  // useEffect(() => {
-  //   console.log("MatrixConfig: ", matrixConfig);
-  // }, [matrixConfig]);
 
   return (
     <View style={styles.container}>
       {/* HEADER */}
       <>
-        <Text style={styles.title}>Registration</Text>
+        <Text style={styles.title}>Matrix</Text>
         <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
       </>
       {/* REGISTRATION */}
-      <SafeAreaView>
-
+      <SafeAreaView style={{ width: '80%'}}>
+        
         <TextInput
           placeholder="Name"
           keyboardType='default'
           keyboardAppearance={colorScheme ?? 'light'}
           onChangeText={value => SetConfig('name', value)}
           style={{
+              ...styles.textInput,
             color: Colors[colorScheme ?? 'light'].text,
             borderColor: Colors[colorScheme ?? 'light'].tint,
-            ...styles.textInput
-          }}
-          />
+          }} />
 
-        <TextInput
-          placeholder="Matrix Width"
-          onChangeText={value => SetConfig('width', Number(value))}
-          keyboardType='numeric'
-          keyboardAppearance={colorScheme ?? 'light'}
-          style={{
-            color: Colors[colorScheme ?? 'light'].text,
-            borderColor: Colors[colorScheme ?? 'light'].tint,
-            ...styles.textInput
-          }}
-          />
+        <VStack style={{'gap': 10, 'marginBottom': 15}}>
+          <TextInput
+            placeholder="Matrix Width"
+            onChangeText={value => SetConfig('width', Number(value))}
+            keyboardType='numeric'
+            keyboardAppearance={colorScheme ?? 'light'}
+            style={{
+              ...styles.textInput,
+              color: Colors[colorScheme ?? 'light'].text,
+              borderColor: Colors[colorScheme ?? 'light'].tint,
+              width: '50%',
+            }}/>
 
-        <TextInput
-          placeholder="Matrix Height"
-          onChangeText={value => SetConfig('height', Number(value))}
-          keyboardType='numeric'
-          keyboardAppearance={colorScheme ?? 'light'}
-          style={{
-            color: Colors[colorScheme ?? 'light'].text,
-            borderColor: Colors[colorScheme ?? 'light'].tint,
-            ...styles.textInput
-          }}
-          />
+          <TextInput
+            placeholder="Matrix Height"
+            onChangeText={value => SetConfig('height', Number(value))}
+            keyboardType='numeric'
+            keyboardAppearance={colorScheme ?? 'light'}
+            style={{
+              ...styles.textInput,
+              color: Colors[colorScheme ?? 'light'].text,
+              borderColor: Colors[colorScheme ?? 'light'].tint,
+              width: '50%'
+            }} />
 
-        <TextInput
-          placeholder="Broker URL"
-          keyboardType='url'
-          onChangeText={value => SetConfig('brokerUrl', value)}
-          keyboardAppearance={colorScheme ?? 'light'}
-          style={{
-            color: Colors[colorScheme ?? 'light'].text,
-            borderColor: Colors[colorScheme ?? 'light'].tint,
-            ...styles.textInput
-          }}
-          />
+        </VStack>
 
-        <TextInput
-          placeholder="Broker PORT"
-          keyboardType='numeric'
-          keyboardAppearance={colorScheme ?? 'light'}
-          onChangeText={value => SetConfig('brokerPort', Number(value))}
-          style={{
-            color: Colors[colorScheme ?? 'light'].text,
-            borderColor: Colors[colorScheme ?? 'light'].tint,
-            ...styles.textInput
-          }}
-          />
+        <HStack>
+          <Text
+            style={{ 'alignSelf': 'center', fontSize: 20}}
+            lightColor={Colors[colorScheme ?? 'light'].text}
+            darkColor={Colors[colorScheme ?? 'dark'].text}
+            >MQTT</Text>
+
+          <View style={{...styles.separator, marginVertical: 15, width: '100%'}} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
+
+          <VStack style={{ 'gap': 5 }}>
+            <TextInput
+              value={matrixConfig.brokerScheme}
+              onChangeText={value => SetConfig('brokerUrl', value)}
+              keyboardAppearance={colorScheme ?? 'light'}
+              style={{
+                ...styles.textInput,
+                color: Colors[colorScheme ?? 'light'].text,
+                borderColor: Colors[colorScheme ?? 'light'].tint,
+                width: '20%',
+                padding: 5,
+                textAlign: 'center',
+              }} />
+
+            <TextInput
+              placeholder="Broker URL"
+              keyboardType='url'
+              onChangeText={value => SetConfig('brokerUrl', value)}
+              keyboardAppearance={colorScheme ?? 'light'}
+              style={{
+                ...styles.textInput,
+                color: Colors[colorScheme ?? 'light'].text,
+                borderColor: Colors[colorScheme ?? 'light'].tint,
+                width: '65%'
+              }} />
+
+            <TextInput
+              value={matrixConfig.brokerPort?.toString()}
+              keyboardType='numeric'
+              keyboardAppearance={colorScheme ?? 'light'}
+              onChangeText={value => SetConfig('brokerPort', Number(value))}
+              style={{
+                ...styles.textInput,
+                color: Colors[colorScheme ?? 'light'].text,
+                borderColor: Colors[colorScheme ?? 'light'].tint,
+                width:'15%',
+                textAlign: 'center',
+                padding: 5
+              }} />
+
+          </VStack>
+        </HStack>
 
       </SafeAreaView>
     </View>
