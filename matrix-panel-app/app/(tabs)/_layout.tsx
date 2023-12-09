b8e9e2040563ded9a6cfcf0c1eabf88046779f39
Sean Jensen <itskolesean@gmail.com>
2023-12-09 13:24:10 +0100

    shees

--- matrix-panel-app/app/(tabs)/_layout.tsx
+++ matrix-panel-app/app/(tabs)/_layout.tsx
@@ -25,8 +25,8 @@ export default function TabLayout() {
       <Tabs.Screen
         name="index"
         options={{
-          title: 'Tab One',
-          tabBarIcon: ({ color }) => <TabBarIcon name="code" color={color} />,
+          title: 'Home',
+          tabBarIcon: ({ color }) => <TabBarIcon name="home" color={color} />,
           headerRight: () => (
             <Link href="/modal" asChild>
               <Pressable>
@@ -44,10 +44,10 @@ export default function TabLayout() {
         }}
       />
       <Tabs.Screen
-        name="two"
+        name="register"
         options={{
-          title: 'Tab Two',
-          tabBarIcon: ({ color }) => <TabBarIcon name="code" color={color} />,
+          title: 'Register',
+          tabBarIcon: ({ color }) => <TabBarIcon name="address-card" color={color} />,
         }}
       />
     </Tabs>
